fb46d150837ecaa948452d74982885e3dac6adf0
SiwooPak <bogus1620@naver.com>
2018-02-08 17:31:54 +0900

    Update PageMaker.java

--- snj/src/main/java/org/snj/domain/PageMaker.java
+++ snj/src/main/java/org/snj/domain/PageMaker.java
@@ -2,18 +2,18 @@ package org.snj.domain;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-
+//화면하단 페이징 처리
 public class PageMaker {
 
-	private int totalCount;
-	private int startPage;
-	private int endPage;
-	private boolean prev;
-	private boolean next;
+	private int totalCount; //게시판의 게시글 총갯수
+	private int startPage; //시작페이지
+	private int endPage; //끝 페이지
+	private boolean prev; // 앞 페이지로
+	private boolean next; // 뒷 페이지로
 	
-	private int displayPageNum = 10;
+	private int displayPageNum = 10; //화면에 표시될 페이지번호
 	
-	private Criteria cri;
+	private Criteria cri; //
 
 
 	public void setCri(Criteria cri) {
@@ -27,19 +27,27 @@ public class PageMaker {
 	}
 
 	private void calcData() {
-		
+		//마지막 페이지 = Math.ceil(현재페이지/페이지 번호의 수)*페이지 번호의 수
+		//만약 현재 페이지가 1이고 화면에 보여질 페이지 번호의 수가 10이면 0.1의 올림의 10을 곱하면 10
 		endPage = (int) (Math.ceil(cri.getPage() / (double)displayPageNum ) * displayPageNum);
 		
+		//시작페이지 = (마지막 페이지 - 페이지번호의 수) + 1
+		//ex)마지막 페이지가 10이고, 페이지번호의 수가 10이면 시작페이지는 1
 		startPage = (endPage - displayPageNum) + 1;
 		
+		//마지막 페이지는 실제 데이터 갯수와 관련이 있어 재계산
 		int tempEndPage = (int)(Math.ceil(totalCount / (double)cri.getPerPageNum()));
 		
 		if(endPage > tempEndPage){
 			endPage = tempEndPage;
 		}
 		
+		//앞페이지로 이동(삼항연산자)
+		// 현재페이지가 1이면  false, 아니면 true
 		prev = startPage ==1 ? false : true;
 		
+		//다음페이지로 이동
+		//마지막 페이지와 페이지의번호수를 곱한 값이 총갯수보다 작으면 이동
 		next = endPage * cri.getPerPageNum() >= totalCount ? false : true;
 		
 	}
