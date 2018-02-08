c7871532a0dfeabe7391c0b2b88328a995fe1102
SiwooPak <bogus1620@naver.com>
2018-02-08 17:09:21 +0900

    Update Criteria.java

--- snj/src/main/java/org/snj/domain/Criteria.java
+++ snj/src/main/java/org/snj/domain/Criteria.java
@@ -1,14 +1,17 @@
 package org.snj.domain;
+
+//페이징
 public class Criteria {
 
-	private int page;	
-	private int perPageNum;
+	private int page;	// 페이지 번호
+	private int perPageNum; // 페이지당 출력 갯수
 	
 	public Criteria(){
 		this.page = 1;
 		this.perPageNum = 12;
 	}
-
+	
+	//페이지 번호가 0보다 작거나 같을시 1페이지로 기본.
 	public void setPage(int page){
 		
 		if(page <= 0){
@@ -19,6 +22,7 @@ public class Criteria {
 		this.page = page;
 	}
 	
+	//페이지당 출력 갯수를 잘못 지정한 경우 강제로 10개씩.
 	public void setPerPageNum(int perPageNum){
 		
 		if(perPageNum <= 0 || perPageNum > 100){
