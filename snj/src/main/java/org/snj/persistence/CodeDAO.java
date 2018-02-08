383e5e4e8a29a409e269b35a8cc436c309595e8b
SiwooPak <bogus1620@naver.com>
2018-02-08 17:45:10 +0900

    Update CodeDAO.java

--- snj/src/main/java/org/snj/persistence/CodeDAO.java
+++ snj/src/main/java/org/snj/persistence/CodeDAO.java
@@ -1,9 +1,10 @@
 package org.snj.persistence;
 
 import org.snj.domain.CodeVO;
-
 import java.util.*;
 
+//게시판
 public interface CodeDAO {
+	//게시판 카테고리 목록
 	public List<CodeVO> retrieveCodeList(Map<String, String> codeParam) throws Exception;
 }
