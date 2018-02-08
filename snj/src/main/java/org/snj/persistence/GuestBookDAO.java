fdbd6eaf9bf2a22733fe56c507b53ba4709541d9
SiwooPak <bogus1620@naver.com>
2018-02-08 17:49:02 +0900

    Update GuestBookDAO.java

--- snj/src/main/java/org/snj/persistence/GuestBookDAO.java
+++ snj/src/main/java/org/snj/persistence/GuestBookDAO.java
@@ -7,19 +7,19 @@ import org.snj.domain.UserVO;
 import org.snj.domain.GuestBookVO;
 
 public interface GuestBookDAO {
-	void createGuest(GuestBookVO vo) throws Exception;
-	void createMember(GuestBookVO vo) throws Exception;
-	boolean delete(Integer gbno) throws Exception;
-	boolean passCheck(Map map) throws Exception;
-	List<GuestBookVO> listCriteria(SearchCriteria cri) throws Exception;
-	int totalCount(SearchCriteria cri) throws Exception;
-	void upAnsnum(Integer grpno) throws Exception;
-	void commentMember(GuestBookVO vo) throws Exception;
-	void commentGuest(GuestBookVO vo) throws Exception;
-	int getGrpno(Integer gbno) throws Exception;
-	void upCommentCount(Integer grpno) throws Exception;
-	GuestBookVO commentRead(int gbno) throws Exception;
-	void updateComment(Map paramMap) throws Exception;
-	void zeroIndex() throws Exception;
-	void setIndex() throws Exception;
+	void createGuest(GuestBookVO vo) throws Exception; //방명록 등록(회원)
+	void createMember(GuestBookVO vo) throws Exception; //방명록 등록(비회원)
+	boolean delete(Integer gbno) throws Exception; // 방명록 삭제
+	boolean passCheck(Map map) throws Exception; // 방명록 글 비빌번호 확인
+	List<GuestBookVO> listCriteria(SearchCriteria cri) throws Exception; //방명록 목록
+	int totalCount(SearchCriteria cri) throws Exception; // 등록된 방명록 총갯수
+	void upAnsnum(Integer grpno) throws Exception; //답글인 경우 +1
+	void commentMember(GuestBookVO vo) throws Exception; //답글 등록(회원)
+	void commentGuest(GuestBookVO vo) throws Exception; //답글 등록(비회원)
+	int getGrpno(Integer gbno) throws Exception; //답글 등록시 부모번호 가져오기
+	void upCommentCount(Integer grpno) throws Exception; //답글 갯수
+	GuestBookVO commentRead(int gbno) throws Exception; //답글 읽어오기
+	void updateComment(Map paramMap) throws Exception; //답글 수정
+	void zeroIndex() throws Exception; //등록번호 초기화
+	void setIndex() throws Exception; //등록번호 정렬
 }
